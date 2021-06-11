bfc8746c8820322b4469a9f280196ef0dfc4b00d
elodie <elodie.siaze@gmail.com>
2021-06-11 14:16:29 +0200

    ajout

---
+++ chat_post.php
@@ -0,0 +1,18 @@
+<?php
+// Connexion à la base de données
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+}
+
+// Insertion du message à l'aide d'une requête préparée
+$req = $bdd->prepare('INSERT INTO chat (pseudo, message) VALUES(?, ?)');
+$req->execute(array($_POST['pseudo'], $_POST['message']));
+
+// Redirection du visiteur vers la page du minichat
+header('Location: minichat.php');
+?>
